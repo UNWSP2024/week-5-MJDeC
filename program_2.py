4324ef264b7c8c43543d627f5e17a2b98eda789b
MJDeC <mjdecaro@students.unwsp.edu>
2024-10-02 19:50:36 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -11,3 +11,13 @@
 # If the answer is correct, a message of congratulations should be displayed.  
 # If the answer is incorrect a message showing the correct answer should be displayed.  
 # The program must use a function that accomplishes part of the needed tasks.
+def math():
+  print(""" 247
++129""")
+  answer=int(input("What is the answer to the math problem?"))
+  if answer==376:
+    print('Yay! You can do addition! I hope you did not have to use your fingers...')
+  else:
+    print('What a loser... The answer is 376.')
+
+math()      
